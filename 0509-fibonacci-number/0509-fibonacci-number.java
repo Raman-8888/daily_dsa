c5a415a1e2d2323f99a4f79ef77ba6215c72e519
Raman Negi <149420966+Raman-8888@users.noreply.github.com>
2026-07-28 15:30:01 +0530

    0 ms (100%) 42.1 MB (52.64%) 07-28-2026 0509-fibonacci-number Easy .java Math

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1)return n;
+
+        
+
+        int last=1;
+        int slast=0;
+        for(int i=2;i<=n;i++)
+        {
+            int ans=last+slast;
+            slast=last;
+            last=ans;
+
+        }
+        return last;
+        
+    }
+}
