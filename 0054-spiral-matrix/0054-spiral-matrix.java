150709908c153796edb01f42af13d89b23c0520d
Raman Negi <149420966+Raman-8888@users.noreply.github.com>
2026-08-12 01:24:42 +0530

    0 ms (100%) 43.3 MB (9.62%) 08-12-2026 0054-spiral-matrix Medium .java Array

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer>res=new ArrayList<>();
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int top=0,left=0;
+        int buttom=m-1,right=n-1;
+
+        while(top<=buttom && left<=right)
+{
+        for(int i=left;i<=right;i++)
+        {
+            res.add(matrix[top][i]);
+        }
+
+        top++;
+
+        for(int i=top;i<=buttom;i++)
+        {
+            res.add(matrix[i][right]);
+        }
+        right--;
+
+
+        if(top<=buttom)
+        {
+
+        for(int i=right;i>=left;i--)
+        {
+            res.add(matrix[buttom][i]);
+
+        }
+        buttom--;
+        }
+
+    if(left<=right)
+    {
+
+        for(int i=buttom;i>=top;i--)
+        {
+            res.add(matrix[i][left]);
+        }
+        left++;
+    }
+
+}
+    return res;
+
+
+
+        
+    }
+}
