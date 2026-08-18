06be251e07f103f16e80a510832c21bb9d462beb
Raman Negi <149420966+Raman-8888@users.noreply.github.com>
2026-08-19 02:04:03 +0530

    29 ms (93.35%) 59.4 MB (32.38%) 08-19-2026 0015-3sum Medium .java Array

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,12 +1,10 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
-        int n=arr.length;
-
-        Arrays.sort(arr);
-
+        
         List<List<Integer>>res=new ArrayList<>();
-
-        for(int i=0;i<n-2;i++)
+        Arrays.sort(arr);
+        
+        for(int i=0;i<arr.length-2;i++)
         {
             if(arr[i]>0)break;
 
@@ -14,30 +12,28 @@ class Solution {
             {
                 continue;
             }
-
             int left=i+1;
-            int right=n-1;
+            int right=arr.length-1;
+
 
             while(left<right)
             {
                 int sum=arr[i]+arr[left]+arr[right];
-                if(sum==0){
+                if(sum==0)
+                {
                     res.add(Arrays.asList(arr[i],arr[left],arr[right]));
 
                     while(left<right && arr[left]==arr[left+1])
-                {
+                    {
+                        left++;
+                    }
+                    while(left<right && arr[right]==arr[right-1])
+                    {
+                        right--;
+                    }
                     left++;
-                }
-                while(left<right && arr[right]==arr[right-1])
-                {
                     right--;
                 }
-                left++;
-                right--;
-
-                }
-                
-
                 else if(sum<0)
                 {
                     left++;
@@ -47,9 +43,10 @@ class Solution {
                 {
                     right--;
                 }
+
             }
         }
         return res;
-        
+
     }
 }
